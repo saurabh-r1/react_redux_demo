79e8ff25deaaadbf36926b640592525dc4f9f9c1
saurabh-r1 <sidlock2@gmail.com>
2023-12-21 15:42:36 +0530

    Subscribing and Dispatching Actions

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import {createStore} from 'redux';
 
 const counterReducer = (state = {counter:0}, action) => {
     if(action.type==='increment'){
-        return {counter: state.counter + 2} ;
+        return {counter: state.counter + 5} ;
     };
 
     if(action.type==='decrement'){
-        return {counter: state.counter - 2} ;
+        return {counter: state.counter - 5} ;
     };
 
     return state;
